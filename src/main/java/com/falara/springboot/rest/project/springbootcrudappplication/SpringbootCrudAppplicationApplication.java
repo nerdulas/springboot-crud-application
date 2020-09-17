a2b950e478f8c55722cb358e064caf8d0e1cc8b7
DESKTOP-KMQN752\elpid <elpidafal@gmail.com>
2020-09-17 20:22:42 +0300

    Create the structure of REST API

---
+++ src/main/java/com/falara/springboot/rest/project/springbootcrudappplication/SpringbootCrudAppplicationApplication.java
@@ -0,0 +1,13 @@
+package com.falara.springboot.rest.project.springbootcrudappplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudAppplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudAppplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/falara/springboot/rest/project/springbootcrudappplication/SpringbootCrudAppplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falara.springboot.rest.project.springbootcrudappplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudAppplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
