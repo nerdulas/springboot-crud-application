2a00b01e514c3a9656bc1a15828f0a26f9282380
DESKTOP-KMQN752\elpid <elpidafal@gmail.com>
2020-09-20 01:02:55 +0300

    Configure H2 database
    - Change database parameters in application.properties
    - Create data.sql file to initialize data for the db at start

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO EMPLOYEE (ID, FIRSTNAME, LASTNAME, EMAIL)
+VALUES (1001, 'Robbie', 'Williams', 'robbiewilliams@gmail.com');
+
+INSERT INTO EMPLOYEE(ID, FIRSTNAME, LASTNAME, EMAIL)
+VALUES (1002, 'George', 'Michael', 'georgemichael@gmail.com');
