ec82eb5f0e763a42a9652b031c63057075f3a319
DESKTOP-KMQN752\elpid <elpidafal@gmail.com>
2020-09-20 03:18:10 +0300

    Add GET method getEmployeeById to EmployeeController
    - Endpoint /employees/{id} to get an Employee entity by its id
    If the Employee doesn't exist, then ResourceNotFoundException happens

--- src/main/java/com/falara/springboot/rest/project/springbootcrudappplication/controller/employee/EmployeeController.java
+++ src/main/java/com/falara/springboot/rest/project/springbootcrudappplication/controller/employee/EmployeeController.java
@@ -1,10 +1,12 @@
 package com.falara.springboot.rest.project.springbootcrudappplication.controller.employee;
 
 import com.falara.springboot.rest.project.springbootcrudappplication.dto.EmployeeDTO;
+import com.falara.springboot.rest.project.springbootcrudappplication.exception.ResourceNotFoundException;
 import com.falara.springboot.rest.project.springbootcrudappplication.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,13 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<EmployeeDTO> getAllEmployees() {
         return this.employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable(value = "id") Long employeeId)
+    throws ResourceNotFoundException {
+        EmployeeDTO employeeDTO = this.employeeService.getEmployeeById(employeeId);
+        return ResponseEntity.ok().body(employeeDTO);
     }
 
     @Autowired
