33ef10de45aead004c244e12c0da7b34591e3d66
DESKTOP-KMQN752\elpid <elpidafal@gmail.com>
2020-09-20 03:50:07 +0300

    Add POST method to EmployeeController
    - Endpoint /employees to create a new Employee

--- src/main/java/com/falara/springboot/rest/project/springbootcrudappplication/controller/employee/EmployeeController.java
+++ src/main/java/com/falara/springboot/rest/project/springbootcrudappplication/controller/employee/EmployeeController.java
@@ -7,15 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeController {
 
+    private static final String ID_STRING = "id";
+    private static final String ID_PATH_STRING = "/{id}";
     EmployeeService employeeService;
 
     @GetMapping("/employees")
@@ -24,10 +30,19 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees/{id}")
-    public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable(value = "id") Long employeeId)
+    public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable(value = ID_STRING) Long employeeId)
     throws ResourceNotFoundException {
         EmployeeDTO employeeDTO = this.employeeService.getEmployeeById(employeeId);
         return ResponseEntity.ok().body(employeeDTO);
+    }
+
+    @PostMapping("/employees")
+    public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody EmployeeDTO employee) {
+        EmployeeDTO savedEmployee = this.employeeService.createEmployee(employee);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path(ID_PATH_STRING)
+                .buildAndExpand(savedEmployee.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
     @Autowired
