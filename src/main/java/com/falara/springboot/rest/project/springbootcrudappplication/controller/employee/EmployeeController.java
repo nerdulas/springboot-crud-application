dca376be018cabcbe1811fb3080698fa38bae3c2
DESKTOP-KMQN752\elpid <elpidafal@gmail.com>
2020-09-20 04:19:40 +0300

    Add PUT method updateEmployee to EmployeeController
    - Endpoint /employees/{id} to update an existing Employee

--- src/main/java/com/falara/springboot/rest/project/springbootcrudappplication/controller/employee/EmployeeController.java
+++ src/main/java/com/falara/springboot/rest/project/springbootcrudappplication/controller/employee/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +44,13 @@ public class EmployeeController {
                 .buildAndExpand(savedEmployee.getId()).toUri();
 
         return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<EmployeeDTO> updateEmployee(@PathVariable(value = ID_STRING) Long employeeId,
+                                                   @RequestBody EmployeeDTO employee) throws ResourceNotFoundException {
+        EmployeeDTO updatedEmployeeDTO = this.employeeService.updateEmployee(employee, employeeId);
+        return ResponseEntity.ok().body(updatedEmployeeDTO);
     }
 
     @Autowired
